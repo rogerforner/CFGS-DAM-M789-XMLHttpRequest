596a7024ba8db1fd29587a428788909ef8db3c1a
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-10-13 11:52:22 +0200

    iniciem XMLHTTPRequest

---
+++ app.js
@@ -0,0 +1,11 @@
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "http://localhost:8000/test-file/file.txt", true);
+    xhttp.send();
+}
